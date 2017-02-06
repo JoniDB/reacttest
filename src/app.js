45d9883871292e1d52e6e228c55bec673005e351
Joni De Borger <joni.de.borger@gmail.com>
2017-02-06 15:57:05 +0100

    added hashHistory

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import jQuery from 'jquery';
-import CommentBox from './components/comment-box';
-import {Router, Route, Redirect} from 'react-router';
+import {hashHistory, Router, Route, Redirect} from 'react-router';
 
 import Layout from './layout/layout';
 
@@ -11,7 +10,7 @@ import PicPage from './pages/picture';
 import VideoPage from './pages/video';
 
 const app = (
-  <Router>
+  <Router history={hashHistory}>
     <Redirect from='/' to='/blog' />
     <Route path='/' component={Layout}>
       <Route path='blog' component={BlogPage}/>
